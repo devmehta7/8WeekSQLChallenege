28f68b912dba255f657aac786734875e469d9c78
Dev <dev.d@simformsolutions.com>
2023-04-20 11:27:13 +0530

    Completed Case Study #7

--- Balanced Tree Clothing Co. (Case Study #7)/questions.sql
+++ Balanced Tree Clothing Co. (Case Study #7)/questions.sql
@@ -361,4 +361,395 @@ JOIN
 WHERE 
 	c1.category_name IS NOT NULL;
 
+-- 9. What is the total transaction “penetration” for each product? 
+-- (hint: penetration = number of transactions where at least 1 quantity of a product was purchased divided by total number of transactions)
 
+SELECT * FROM sales;
+
+WITH cte
+AS
+(
+	SELECT
+		txn_id, product_name,
+		count(prod_id) AS cnt
+	FROM 
+		sales s
+	JOIN 
+		product_details pd ON s.prod_id = pd.product_id
+	GROUP BY 
+		txn_id, product_name
+)
+SELECT
+	product_name,
+	COUNT(product_name)*100.0/(SELECT count(DISTINCT txn_id) FROM sales) AS penetration_per_product
+FROM
+	cte	
+GROUP BY 
+	product_name
+ORDER BY 
+	penetration_per_product DESC;
+
+-- 10. What is the most common combination of at least 1 quantity of any 3 products in a 1 single transaction?
+
+WITH products 
+ AS(
+      SELECT
+        txn_id,
+        product_name
+      FROM
+        sales AS s
+        JOIN product_details AS pd ON s.prod_id = pd.product_id
+),
+cte_2
+AS
+(
+    SELECT
+      p.product_name AS product_1,
+      p1.product_name AS product_2,
+      p2.product_name AS product_3,
+      COUNT(*) AS times_bought_together,
+      ROW_NUMBER() OVER(
+        ORDER BY
+          COUNT(*) DESC
+      ) AS rank
+    FROM
+      products AS p
+      JOIN products AS p1 ON p.txn_id = p1.txn_id
+      AND p.product_name != p1.product_name
+      AND p.product_name < p1.product_name
+      JOIN products AS p2 ON p.txn_id = p2.txn_id
+      AND p.product_name != p2.product_name
+      AND p1.product_name != p2.product_name
+      AND p.product_name < p2.product_name
+      AND p1.product_name < p2.product_name
+    GROUP BY
+      p.product_name,
+      p1.product_name,
+      p2.product_name
+ )
+ SELECT 
+  product_1,
+  product_2,
+  product_3,
+  times_bought_together
+FROM
+	cte_2
+WHERE 
+	rank=1;
+GO
+----------------------
+-- Reporting Challenge
+----------------------
+
+CREATE PROCEDURE ReportingChallenge
+	@month int
+AS
+	-- set @month = 1;
+	
+	-- 1.
+	SELECT
+		product_name,
+		amount
+	FROM
+		product_details
+	JOIN
+	(SELECT
+		TOP 3
+		prod_id,
+		SUM(qty * price) AS amount
+	FROM
+		sales
+	WHERE
+		DATEPART(MONTH, start_txn_time) = @month
+	GROUP BY 
+		prod_id
+	ORDER BY
+		amount DESC) t ON t.prod_id =product_details.product_id
+	ORDER BY 
+		amount DESC;
+	-- 2. 
+	SELECT 
+		segment_name,
+		SUM(qty) AS total_quantity,
+		SUM(qty * s.price) AS revenue,
+		SUM(qty * discount *0.01 * s.price) AS total_discount
+	FROM
+		sales s
+	JOIN 
+		product_details pd ON s.prod_id = pd.product_id
+	WHERE
+		DATEPART(MONTH, start_txn_time) = @month
+	GROUP BY
+		segment_name;
+	
+	-- 3. 
+	
+	WITH cte
+	AS
+	(
+		SELECT
+			segment_name,
+			product_name,
+			SUM(qty) as qty_sold,
+			DENSE_RANK() OVER(PARTITION BY segment_name ORDER BY SUM(qty) DESC) AS rnk
+		FROM
+			sales s
+		JOIN 
+			product_details pd ON s.prod_id = pd.product_id
+		WHERE
+			DATEPART(MONTH, start_txn_time) = @month 
+		GROUP BY 
+			segment_name, product_name
+	)
+	SELECT
+		segment_name,
+		product_name,
+		qty_sold,
+		rnk
+	FROM
+		cte
+	WHERE
+		rnk = 1;
+
+	-- 4.
+
+	SELECT 
+		category_name,
+		SUM(qty) AS total_quantity,
+		SUM(qty * s.price) AS revenue,
+		SUM(qty * discount *0.01 * s.price) AS total_discount
+	FROM
+		sales s
+	JOIN 
+		product_details pd ON s.prod_id = pd.product_id
+	WHERE
+		DATEPART(MONTH, start_txn_time) = @month
+	GROUP BY
+		category_name;
+
+	-- 5. What is the top selling product for each category?
+
+	WITH cte
+	AS
+	(
+		SELECT
+			category_name,
+			product_name,
+			SUM(qty) as qty_sold,
+			ROW_NUMBER() OVER(PARTITION BY category_name ORDER BY COUNT(qty) DESC) AS rnk
+		FROM
+			sales s
+		JOIN 
+			product_details pd ON s.prod_id = pd.product_id
+		WHERE
+			DATEPART(MONTH, start_txn_time) = @month
+		GROUP BY 
+			category_name, product_name
+	)
+	SELECT
+		category_name,
+		product_name,
+		qty_sold
+	FROM
+		cte
+	WHERE
+		rnk = 1;
+
+	-- 6. 
+
+	WITH cte
+	AS
+	(
+		SELECT
+			segment_name,
+			product_name,
+			SUM(qty * s.price) AS revenue
+		FROM
+			sales s
+		JOIN 
+			product_details pd ON s.prod_id = pd.product_id
+		WHERE
+			DATEPART(MONTH, start_txn_time) = @month
+		GROUP BY 
+			ROLLUP(segment_name, product_name)
+	)
+	SELECT 
+		c1.segment_name,
+		c1.product_name,
+		CAST(c1.revenue*100.0/c2.revenue AS NUMERIC(5,2)) AS segment_perc
+	FROM 
+		cte	c1
+	JOIN
+		cte c2 ON c1.segment_name = c2.segment_name AND c2.product_name IS NULL
+	WHERE 
+		c1.product_name IS NOT NULL
+	ORDER BY
+		segment_name,
+		segment_perc DESC;
+
+	-- 7.
+
+	WITH cte
+	AS
+	(
+		SELECT
+			category_name,
+			segment_name,
+			SUM(qty * s.price) AS revenue
+		FROM
+			sales s
+		JOIN 
+			product_details pd ON s.prod_id = pd.product_id
+		WHERE
+			DATEPART(MONTH, start_txn_time) = @month
+		GROUP BY 
+			ROLLUP(category_name, segment_name)
+	)
+	SELECT 
+		c1.category_name,
+		c1.segment_name,
+		CAST(c1.revenue*100.0/c2.revenue AS NUMERIC(5,2)) AS category_perc
+	FROM 
+		cte	c1
+	JOIN
+		cte c2 ON c1.category_name = c2.category_name AND c2.segment_name IS NULL
+	WHERE 
+		c1.segment_name IS NOT NULL
+	ORDER BY
+		category_name,
+		category_perc DESC;
+
+	-- 8. 
+
+	WITH cte
+	AS
+	(
+		SELECT
+			category_name,
+			SUM(qty*s.price) AS revenue
+		FROM
+			sales s
+		JOIN 
+			product_details pd ON s.prod_id = pd.product_id
+		WHERE
+			DATEPART(MONTH, start_txn_time) = @month
+		GROUP BY 
+			ROLLUP(category_name)
+	)
+	SELECT 
+		c1.category_name,
+		CAST(ROUND(c1.revenue * 100.0 / c2.revenue, 2) AS NUMERIC(5,2)) AS revenue_perc
+	FROM 
+		cte	c1
+	JOIN
+		cte c2 ON c2.category_name IS NULL
+	WHERE 
+		c1.category_name IS NOT NULL;
+
+	-- 9.
+
+	WITH cte
+	AS
+	(
+		SELECT
+			txn_id, product_name,
+			count(prod_id) AS cnt
+		FROM 
+			sales s
+		JOIN 
+			product_details pd ON s.prod_id = pd.product_id
+		WHERE
+			DATEPART(MONTH, start_txn_time) = @month
+		GROUP BY 
+			txn_id, product_name
+	)
+	SELECT
+		product_name,
+		COUNT(product_name)*100.0/(SELECT count(DISTINCT txn_id) FROM sales) AS penetration_per_product
+	FROM
+		cte	
+	GROUP BY 
+		product_name
+	ORDER BY 
+		penetration_per_product DESC;
+
+	-- 10.
+
+	WITH products 
+	AS(
+		SELECT
+			txn_id,
+			product_name
+		FROM
+		sales AS s
+		JOIN product_details AS pd ON s.prod_id = pd.product_id
+		WHERE
+			DATEPART(MONTH, start_txn_time) = @month		
+	),
+	cte_2
+	AS
+	(
+		SELECT
+		  p.product_name AS product_1,
+		  p1.product_name AS product_2,
+		  p2.product_name AS product_3,
+		  COUNT(*) AS times_bought_together,
+		  ROW_NUMBER() OVER(
+			ORDER BY
+			  COUNT(*) DESC
+		  ) AS rank
+		FROM
+		  products AS p
+		  JOIN products AS p1 ON p.txn_id = p1.txn_id
+		  AND p.product_name != p1.product_name
+		  AND p.product_name < p1.product_name
+		  JOIN products AS p2 ON p.txn_id = p2.txn_id
+		  AND p.product_name != p2.product_name
+		  AND p1.product_name != p2.product_name
+		  AND p.product_name < p2.product_name
+		  AND p1.product_name < p2.product_name
+		GROUP BY
+		  p.product_name,
+		  p1.product_name,
+		  p2.product_name
+	 )
+	 SELECT 
+	  product_1,
+	  product_2,
+	  product_3,
+	  times_bought_together
+	FROM
+		cte_2
+	WHERE 
+		rank=1;
+GO
+
+EXEC ReportingChallenge 2;
+
+-----------------
+-- Bonus Question
+-----------------
+SELECT * FROM 
+product_hierarchy;
+
+SELECT * FROM 
+product_prices ;
+
+SELECT * FROM 
+product_details ;
+
+
+SELECT 
+	product_id, 
+	price,  
+	CONCAT(ph1.level_text, ' ',ph2.level_text, ' ', ph3.level_text) AS product_name,
+	ph2.id as parentId,
+	ph2.id AS segmentId,
+	ph2.id AS style_id,
+	ph3.level_text AS category_name,
+	ph2.level_text AS segment_name,
+	ph1.level_text AS style_name
+FROM product_hierarchy ph1
+JOIN product_hierarchy ph2 ON ph1.parent_id = ph2.id
+JOIN product_hierarchy ph3 ON ph2.parent_id = ph3.id 
+LEFT JOIN product_prices pp ON pp.id = ph1.id
